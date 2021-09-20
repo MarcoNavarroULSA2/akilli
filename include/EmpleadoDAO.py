354e20d8cb52852eb1a53ae9234181ca8b55d879
Name example <example@hotmail.com>
2021-09-20 01:38:34 -0500

    Se le cambio el nombre al archivo

--- include/EmpleadoDAO.py
+++ include/EmpleadoDAO.py
@@ -1,20 +1,23 @@
 from flask import json
 import include.conexion as cnx 
-from include.VO import EmpleadoVO
+from include.EmpleadoVO import EmpleadoVO
 
 class EmpleadoDAO:
     def __init__(self):
         self.__tabla = "Empleado"
 
-    def selectALL(self):
+    def findEmail(self, email):
         try:
+            print(email)
             conn=cnx.mysql.connect()
             cursor=conn.cursor()
-            cursor.execute('SELECT * FROM '+ self.__tabla +' ORDER BY IdUsuario DESC')
+            query_select=('SELECT Correo FROM Login_Empleado WHERE Correo = %s') 
+            values=(email) 
+            cursor.execute(query_select, values)
             data=cursor.fetchall()
             listaVO=[]
             for fila in data:
-                vo = EmpleadoVO(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5] )
+                vo = LogInVO(fila[0], fila[1])
                 listaVO.append(vo)
             return listaVO
         except Exception as e:
