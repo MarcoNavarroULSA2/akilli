7525299373249d1c9b77d47459ed83ab75c09220
manavarro2000 <86426056+manavarro2000@users.noreply.github.com>
2021-10-17 23:23:52 -0500

    creación de eventos

---
+++ include/eventoVO.py
@@ -0,0 +1,22 @@
+class LogInVO:
+    def __init__(self, idEvento, nombreEvento, idUsuario, tiempo):
+        self.__idEvento = idEvento
+        self.__nombreEvento = nombreEvento
+        self.__idUsuario = idUsuario
+        self.__tiempo = tiempo
+
+    def setAll(self, idEvento, nombreEvento, idUsuario, tiempo ):        
+        self.__idEvento = idEvento
+        self.__nombreEvento = nombreEvento
+        self.__idUsuario = idUsuario
+        self.__tiempo = tiempo
+
+    def getIdEvento(self):
+        return self.__idEvento
+    def getNombreEvento(self):
+        return self.__nombreEvento
+    def getIdUsuario(self):
+        return self.__idUsuario
+    def getTiempo(self):
+        return self.__tiempo
+    
